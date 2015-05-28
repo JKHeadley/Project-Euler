fd9a91f52eb0abb812c7cde91fd4d86a1e2b20e4
Justin Headley <headl004@crimson.ua.edu>
2015-05-28 14:49:28 -0500

    Reorganized and added a stopwatch to measure problem solving times.

--- Project Euler/Project Euler/Program.cs
+++ Project Euler/Project Euler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Project_Euler
 {
@@ -10,9 +11,26 @@ namespace Project_Euler
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Problem 1:\n");
-            new Problem1().Solve();
+            Console.WriteLine("Enter problem # to solve (1-517):");
+            int probNum = Int32.Parse(Console.ReadLine());
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            switch (probNum)
+            {
+                case 1:
+                    Console.WriteLine("Problem 1:\n");
+                    new Problem1().Solve();
+                    break;
+                case 2:
+                    Console.WriteLine("Problem 2:\n");
+                    new Problem2().Solve();
+                    break;
+                default:
+                    Console.WriteLine("Problem not yet solved:\n");
+                    break;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Finished in " + stopwatch.Elapsed + " seconds");
             Console.Read();
         }
     }
